42e6a53240f80d996983241c31f930335302f685
Aashman15 <aashmanthing1@gmail.com>
2025-12-05 14:41:01 +0545

    added a job to delete unsubmitted practices that are older than one day

--- src/main/java/com/aashman/learnmate/features/practice/PracticeJobs.java
+++ src/main/java/com/aashman/learnmate/features/practice/PracticeJobs.java
@@ -1,10 +1,38 @@
 package com.aashman.learnmate.features.practice;
 
+import java.time.Instant;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.aashman.learnmate.features.practice.entities.Practice;
+import com.aashman.learnmate.features.practice.enums.PracticeStatus;
+import com.aashman.learnmate.features.practice.repositories.PracticeRepository;
 
 @Component
 public class PracticeJobs {
-    void deleteUnsubmittedPracticeThatIsOlderThanOneDay() {
 
+    @Autowired
+    private PracticeRepository practiceRepository;
+
+    // After 5 minute delete
+    @Scheduled(fixedDelay = 300000)
+    void deleteUnsubmittedPracticeThatIsOlderThanOneDay() {
+        System.out.println("Clearing unsubmitted practices");
+    Specification<Practice> practiceSpecification = Specification.where(null);
+
+    practiceSpecification = practiceSpecification.and((root, query, cb) -> {
+                    Instant oneDayAgo = Instant.now().minusSeconds(24 * 60 * 60); 
+                    return cb.lessThan(root.get("startTime"), oneDayAgo);
+                    
+    });
+
+    practiceSpecification =  practiceSpecification.and((root, query, cb) -> cb.and(cb.equal(root.get("status"), PracticeStatus.STARTED)));
+
+    List<Practice> practices =   this.practiceRepository.findAll(practiceSpecification);
+    practices.forEach(this.practiceRepository::delete);
     }
 }
--- src/main/java/com/aashman/learnmate/features/practice/repositories/PracticeRepository.java
+++ src/main/java/com/aashman/learnmate/features/practice/repositories/PracticeRepository.java
@@ -7,13 +7,14 @@ import java.util.List;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import com.aashman.learnmate.features.practice.enums.PracticeStatus;
 
 
 
 @Repository
-public interface PracticeRepository extends JpaRepository<Practice, Long> {
+public interface PracticeRepository extends JpaRepository<Practice, Long>, JpaSpecificationExecutor<Practice> {
     default Practice findByIdOrThrow(Long practiceId) {
         return  this.findById(practiceId).orElseThrow(() -> new NotFoundException("Practice", practiceId));
     }
