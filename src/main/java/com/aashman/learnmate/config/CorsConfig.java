94b6dd47cadf638586cfde3ceb52a89e2f0f23a8
Aashman15 <aashmanthing1@gmail.com>
2025-12-14 10:21:11 +0545

    allowing all origins  - cors

--- src/main/java/com/aashman/learnmate/config/CorsConfig.java
+++ src/main/java/com/aashman/learnmate/config/CorsConfig.java
@@ -10,14 +10,14 @@ public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return  new WebMvcConfigurer() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-              registry.addMapping("/**")
-                      .allowedOrigins("http://localhost:3000")
-                      .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS")
-                      .allowedHeaders("*")
-                      .allowCredentials(true);
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
         };
     }
